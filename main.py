e9db3975b97099dca861b6145384ce5b590377c8
Natalia Książek <nksiazek@student.agh.edu.pl>
2022-12-21 15:01:03 +0100

    aaa

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from algorithm import *
 from structure import *
 from utils import print_summary
@@ -13,6 +15,8 @@ with open('Produkty.txt', 'r', encoding='utf8') as f:
 # dodawanie hurtowni
 with open('Hurtownie.txt', 'r', encoding='utf8') as f:
     for lines in f.readlines():
+        lst = [random.randint(5, 18) for _ in range(len(f.readlines())+1)]
+        lst[nasz_sklep.max_id_hurt] = 0
         nasz_sklep.add_wholesaler(Wholesaler(lines[:-1], nasz_sklep.max_id_hurt))
 
 # dodawanie produktów do hurtowni
