3d2004790356244dd778df24160fd7453d5b9bba
Natalia Książek <nksiazek@student.agh.edu.pl>
2022-12-26 01:53:03 +0100

    gui vol 2 with azure

--- gui_tests.py
+++ gui_tests.py
@@ -1,75 +1,125 @@
-# WERSJA TESTOWA - coś tam wypisuje ale nie za wiele
-
 import tkinter as tk
+import tkinter.ttk as ttk
 from tkinter import filedialog
 
 # Set window
-window = tk.Tk()
-window.geometry('600x550')
+root = tk.Tk()
+root.title('Sklep')
+root.resizable(width=False, height=False)  # rozmiar okna jest stały
 
-# Set labels
-tk.Label(text="Parametry:", fg="black", bg="white", font=15).place(relx=0.1, rely=0.1)
-tk.Label(text="Prawdopodobieństwo:", fg="black", bg="white", font=('Arial', 12)).place(relx=0.1, rely=0.2)
-tk.Label(text="Mutacji 1", fg="black", bg="white").place(relx=0.1, rely=0.25)
-tk.Label(text="Mutacji 2", fg="black", bg="white").place(relx=0.1, rely=0.3)
-tk.Label(text="Mutacji 3", fg="black", bg="white").place(relx=0.1, rely=0.35)
-tk.Label(text="Krzyżowania", fg="black", bg="white").place(relx=0.1, rely=0.4)
-tk.Label(text="Liczebność populacji", fg="black", bg="white").place(relx=0.5, rely=0.2)
-tk.Label(text="Liczba iteracji", fg="black", bg="white").place(relx=0.5, rely=0.28)
-aa = tk.Label(text="Hurtownie")
-aa.place(relx=0.4, rely=0.6)
+window_height = 500
+window_width = 800
+root.geometry("{}x{}".format(window_width, window_height))
 
-# Inputs
-wid_m1 = tk.Entry(fg="black", bg="white", width=8)
-wid_m2 = tk.Entry(fg="black", bg="white", width=8)
-wid_m3 = tk.Entry(fg="black", bg="white", width=8)
-wid_k = tk.Entry(fg="black", bg="white", width=8)
-wid_pop = tk.Entry(fg="black", bg="white", width=8)
-wid_it = tk.Entry(fg="black", bg="white", width=8)
-wid_m1.place(relx=0.23, rely=0.25)
-wid_m2.place(relx=0.23, rely=0.3)
-wid_m3.place(relx=0.23, rely=0.35)
-wid_k.place(relx=0.23, rely=0.4)
-wid_pop.place(relx=0.7, rely=0.2)
-wid_it.place(relx=0.7, rely=0.28)
 
-# Actions
+def center_screen():
+    """ gets the coordinates of the center of the screen """
+    global screen_height, screen_width, x_cordinate, y_cordinate
+
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x_cordinate = int((screen_width / 2) - (window_width / 2))
+    y_cordinate = int((screen_height / 2) - (window_height / 2))
+    root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+
+
+center_screen()
+
+# Set Azure style
+style = ttk.Style(root)
+root.tk.call('source', 'azure/azure.tcl')
+style.theme_use('azure')
+style.configure("Accentbutton", foreground='white')
+
+# Variables
 global params
 global hurtownie
 global cars
 global products
+m1P = tk.IntVar()
+m2P = tk.IntVar()
+m3P = tk.IntVar()
+kP = tk.IntVar()
+popSize = tk.IntVar()
+iterations = tk.IntVar()
+wData = tk.IntVar()
+cData = tk.IntVar()
+pData = tk.IntVar()
 
 
 def get_params() -> None:
     # Będzie można to pominąć i mieć cały czas dostęp do tych paramterów pod postacią x.get()
-    m1 = wid_m1.get()
-    m2 = wid_m2.get()
-    m3 = wid_m3.get()
-    k = wid_k.get()
-    pop = wid_pop.get()
-    it = wid_it.get()
+    # Ale chyba lepiej zostać przy zmiennych globalnych
+    global params
+
+    m1 = inM1.get()
+    m2 = inM2.get()
+    m3 = inM3.get()
+    k = inK.get()
+    pop = inPop.get()
+    it = inIt.get()
     params = [m1, m2, m3, k, pop, it]
+
+
+# Frames
+# TODO: zmiana czcionki i ramek frame 'Parametry' oraz 'Dane'
+paramsFrame = ttk.LabelFrame(root, text='Parametry', width=250, height=360)
+paramsFrame.place(x=20, y=12)
+propFrame = ttk.LabelFrame(paramsFrame, text='Prawdopodobieństwo', width=200, height=215)
+propFrame.place(x=20, y=15)
+dataFrame = ttk.LabelFrame(root, text='Dane', width=450, height=300)
+dataFrame.place(x=300, y=12)
+
+# Labels
+ttk.Label(propFrame, text="Mutacji 1").place(x=20, y=15)
+ttk.Label(propFrame, text="Mutacji 2").place(x=20, y=62)
+ttk.Label(propFrame, text="Mutacji 3").place(x=20, y=109)
+ttk.Label(propFrame, text="Krzyżowania").place(x=20, y=156)
+ttk.Label(paramsFrame, text="Liczebność\npopulacji").place(x=40, y=245)
+ttk.Label(paramsFrame, text="Liczba iteracji").place(x=40, y=303)
+pathW = tk.Label(dataFrame, text="Filepath: -")
+pathW.place(x=20, y=60)
+pathC = tk.Label(dataFrame, text="Filepath: -")
+pathC.place(x=20, y=145)
+pathP = tk.Label(dataFrame, text="Filepath: -")
+pathP.place(x=20, y=230)
+
+# Inputs
+inM1 = ttk.Entry(propFrame, width=8)
+inM1.place(x=110, y=8)
+inM2 = ttk.Entry(propFrame, width=8)
+inM2.place(x=110, y=55)
+inM3 = ttk.Entry(propFrame, width=8)
+inM3.place(x=110, y=102)
+inK = ttk.Entry(propFrame, width=8)
+inK.place(x=110, y=149)
+inPop = ttk.Entry(paramsFrame, width=8)
+inPop.place(x=130, y=245)
+inIt = ttk.Entry(paramsFrame, width=8)
+inIt.place(x=130, y=295)
 
 
 # Reading .txt files
 def browser_wholsel():
+    global hurtownie
     hurtownie = filedialog.askopenfilename(initialdir="/", title="Select a File",
                                            filetypes=(("Text files", "*.txt*"), ("all files", "*.*")))
     # if hurtownie:
     #     with open(hurtownie, mode="r", encoding="utf-8") as input_file:
     #         for line in input_file.readlines():
     #             print(line)
-    aa.configure(text='Filepath:'+hurtownie)
+    pathW.configure(text='Filepath:' + hurtownie)
 
 
 # Buttons
-get_paramsB = tk.Button(window, text='Zapisz parametry', command=get_params)
-get_paramsB.place(relx=0.5, rely=0.4)
-get_whols = tk.Button(window, text='Wgraj hurtownie', command=browser_wholsel)
-get_whols.place(relx=0.2, rely=0.6)
-get_whols = tk.Button(window, text='Wgraj samochody', command=browser_wholsel)
-get_whols.place(relx=0.2, rely=0.7)
-get_whols = tk.Button(window, text='Wgraj produkty', command=browser_wholsel)
-get_whols.place(relx=0.2, rely=0.8)
+# TODO: napisanie funkcji browser dla samochodów oraz produktów
+get_paramsB = ttk.Button(root, text='Zapisz parametry', style='Accentbutton', command=get_params)
+get_paramsB.place(x=80, y=400)
+get_whols = ttk.Button(dataFrame, text='Wgraj hurtownie', style='Accentbutton', command=browser_wholsel)
+get_whols.place(x=20, y=15)
+get_whols = ttk.Button(dataFrame, text='Wgraj samochody', style='Accentbutton', command=browser_wholsel)
+get_whols.place(x=20, y=100)
+get_whols = ttk.Button(dataFrame, text='Wgraj produkty', style='Accentbutton', command=browser_wholsel)
+get_whols.place(x=20, y=185)
 
-window.mainloop()
+root.mainloop()
